a55d9bae37753e834a8d72c87224d928ea66b0f5
Rahul Arora <rahularo71171@gmail.com>
2023-08-20 10:23:59 +0530

    Styled the navbar

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,20 +1,22 @@
-import React from 'react'
+import React from "react";
 
 export default function alert(props) {
-    return (
-        <div style={{height: '60px'}}>
-
-            {props.mess && 
-                 <div className={`alert alert-${props.mess.type} alert-dismissible fade show`} role="alert">
-                     <strong>{props.mess.type}:</strong> {props.mess.msg}
-                     <button
-                         type="button"
-                         className="btn-close"
-                         data-bs-dismiss="alert"
-                         aria-label="Close"
-                     />
-                 </div>}
+  return (
+    <div style={{ height: "30px" }}>
+      {props.mess && (
+        <div
+          className={`alert alert-${props.mess.type} alert-dismissible fade show`}
+          role="alert"
+        >
+          <strong>{props.mess.type}:</strong> {props.mess.msg}
+          <button
+            type="button"
+            className="btn-close"
+            data-bs-dismiss="alert"
+            aria-label="Close"
+          />
         </div>
-        
-    )
+      )}
+    </div>
+  );
 }
