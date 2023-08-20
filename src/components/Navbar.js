7f06917c326a74f74511fd18d64c90bb1eb9052a
Rahul Arora <rahularo71171@gmail.com>
2023-08-20 09:54:27 +0530

    Changed the logos

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,45 +1,62 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import PropTypes from "prop-types";
+import { NavLink, Link } from "react-router-dom";
+import logo from '../images/wordmagic.png';
 
 export default function Navbar(props) {
-    return (
-        <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
-            <div className="container-fluid">
-
-                <NavLink className="navbar-brand" to="/">{props.title}</NavLink>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <NavLink className="nav-link" aria-current="page" to="/">Home</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link" aria-current="page" to="/about">About</NavLink>
-                        </li>
-                    </ul>
-
-                </div>
-            </div>
-            <div className="form-check form-switch">
-                <input
-                    className="form-check-input checkout"
-                    type="checkbox"
-                    role="switch"
-                    id="flexSwitchCheckChecked"
-                    defaultChecked=""
-                    onClick={props.toggleMode}
-                />
-
-            </div>
-
-        </nav>
-    )
+  return (
+    <nav
+      className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
+    >
+      <div className="container-fluid">
+        <Link to="/" className="navbar-brand">
+          <img
+            src={logo}
+            style={{width:"150px"}}
+            className="nav-logo img-fluid"
+            alt="Logo"
+          />
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink className="nav-link" aria-current="page" to="/">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" aria-current="page" to="/about">
+                About
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="form-check form-switch">
+        <input
+          className="form-check-input checkout"
+          type="checkbox"
+          role="switch"
+          id="flexSwitchCheckChecked"
+          defaultChecked=""
+          onClick={props.toggleMode}
+        />
+      </div>
+    </nav>
+  );
 }
 
 Navbar.propTypes = {
-    title: PropTypes.string
-}
-
+  title: PropTypes.string,
+};
